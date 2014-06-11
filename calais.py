78d59c41c91b45c79321b15a4ea92f64cf8525e5
Sergey Vorontsov <cepera.voron@gmail.com>
2014-06-11 11:49:32 +0300

    revert to origin

--- calais.py
+++ calais.py
@@ -7,7 +7,6 @@ Last-Update: 01/12/2009
 import httplib, urllib, re
 import simplejson as json
 from StringIO import StringIO
-from xml.sax.saxutils import escape
 
 PARAMS_XML = """
 <c:params xmlns:c="http://s.opencalais.com/1/pred/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"> <c:processingDirectives %s> </c:processingDirectives> <c:userDirectives %s> </c:userDirectives> <c:externalMetadata %s> </c:externalMetadata> </c:params>
@@ -35,10 +34,10 @@ class Calais():
         self.user_directives["submitter"]=submitter
 
     def _get_params_XML(self):
-        return PARAMS_XML % (" ".join('c:%s="%s"' % (k,escape(v)) for (k,v) in self.processing_directives.items() if v), " ".join('c:%s="%s"' % (k,escape(v)) for (k,v) in self.user_directives.items() if v), " ".join('c:%s="%s"' % (k,escape(v)) for (k,v) in self.external_metadata.items() if v))
+        return PARAMS_XML % (" ".join('c:%s="%s"' % (k,v) for (k,v) in self.processing_directives.items() if v), " ".join('c:%s="%s"' % (k,v) for (k,v) in self.user_directives.items() if v), " ".join('c:%s="%s"' % (k,v) for (k,v) in self.external_metadata.items() if v))
 
     def rest_POST(self, content):
-        params = urllib.urlencode({'licenseID':self.api_key, 'content':content.encode('ascii', 'xmlcharrefreplace'), 'paramsXML':self._get_params_XML()})
+        params = urllib.urlencode({'licenseID':self.api_key, 'content':content, 'paramsXML':self._get_params_XML()})
         headers = {"Content-type":"application/x-www-form-urlencoded"}
         conn = httplib.HTTPConnection("api.opencalais.com:80")
         conn.request("POST", "/enlighten/rest/", params, headers)
@@ -115,7 +114,7 @@ class CalaisResponse():
     
     def __init__(self, raw_result):
         try:
-            self.raw_response = json.load(StringIO(raw_result.decode('utf-8', "[removed]")), encoding="utf-8")
+            self.raw_response = json.load(StringIO(raw_result))
         except:
             raise ValueError(raw_result)
         self.simplified_response = self._simplify_json(self.raw_response)
